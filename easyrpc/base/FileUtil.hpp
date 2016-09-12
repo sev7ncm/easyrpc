1bf9b8b6172489640c762e5ac730b106ad7e37cd
chxuan <787280310@qq.com>
2016-09-12 10:00:53 +0800

    支持linux和win平台

--- easyrpc/base/FileUtil.hpp
+++ easyrpc/base/FileUtil.hpp
@@ -1,18 +1,24 @@
 #ifndef _FILEUTIL_H
 #define _FILEUTIL_H
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifdef _WIN32
+#include <Windows.h>
+#include <direct.h>
+#else
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/sysinfo.h>
-
-#ifndef PATH_MAX
-#define PATH_MAX        1024        // 默认最大路径长度
 #endif
+
+#include <io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+
+#define PATH_MAX 1024      
 
 namespace easyrpc
 {
@@ -22,22 +28,39 @@ class FileUtil
 public:
     static std::string currentWorkPath()
     {
+#ifdef _WIN32
+		char buf[MAX_PATH];
+		GetCurrentDirectoryA(sizeof(buf), buf);  
+		return buf;
+#else
         char buf[PATH_MAX] = {'\0'};
         if (getcwd(buf, sizeof (buf) - 1) != nullptr)
         {
             return buf;
         }
         return "";
+#endif
     }
 
     static bool setCurrentWorkPath(const std::string& path)
     {
-        int ret = chdir(path.c_str());
-        return ret == -1 ? false : true;
+#ifdef _WIN32
+		BOOL ret =  SetCurrentDirectoryA(path.c_str());  
+		return ret == TRUE ? true : false;
+#else
+		int ret = chdir(path.c_str());
+		return ret == -1 ? false : true;
+#endif
     }
 
     static std::string currentExePath()
     {
+#ifdef _WIN32
+		char path[MAX_PATH + 1] = { '\0' };
+		::GetModuleFileNameA(NULL, path, MAX_PATH);
+		(strrchr(path, '\\'))[1] = 0;
+		return path;
+#else
         char buf[PATH_MAX] = {'\0'};
 
         int ret = readlink("/proc/self/exe", buf, PATH_MAX);
@@ -54,10 +77,17 @@ public:
         }
 
         return path.substr(0, pos);
+#endif
     }
 
     static std::string currentExeName()
     {
+#ifdef _WIN32
+		char path[MAX_PATH + 1] = { '\0' };
+		::GetModuleFileNameA(NULL, path, MAX_PATH);
+		(strrchr(path, '\\'))[1] = 0;
+		return path;
+#else
         char buf[PATH_MAX] = {'\0'};
 
         int ret = readlink("/proc/self/exe", buf, PATH_MAX);
@@ -74,12 +104,12 @@ public:
         }
 
         return path.substr(pos + 1, path.size() - 1);
+#endif
     }
 
     static bool isExists(const std::string& path)
     {
-        // F_OK 用于判断文件是否存在
-        int ret = access(path.c_str(), F_OK);
+        int ret = _access(path.c_str(), 0);
         return ret == -1 ? false : true;
     }
 
@@ -96,14 +126,14 @@ public:
         }
 
         std::string dirPath = path;
-        if (dirPath[dirPath.size() - 1] != '/')
+        if (dirPath[dirPath.size() - 1] != '/' || dirPath[dirPath.size() - 1] != '\\')
         {
             dirPath += '/';
         }
 
         for (std::size_t i = 0; i < dirPath.size(); ++i)
         {
-            if (dirPath[i] == '/') 
+            if (dirPath[i] == '/' || dirPath[i] == '\\')
             {
                 if (i == 0)
                 {
@@ -113,10 +143,14 @@ public:
                 std::string tempPath = dirPath.substr(0, i);
                 if (!isExists(tempPath))
                 {
-                    if (::mkdir(tempPath.c_str(), 0755) == -1)
-                    {
-                        return false;
-                    }
+#ifdef _WIN32
+					if (_mkdir(tempPath.c_str()) != 0)
+#else
+					if (::mkdir(tempPath.c_str(), 0755) != 0)
+#endif
+					{
+						return false;
+					}
                 }
             }
         }
@@ -142,4 +176,5 @@ public:
 };
 
 }
+
 #endif
